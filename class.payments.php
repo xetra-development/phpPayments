9398b77a1489d91f1b9c212fb49b3245e171d346
Blake Stevenson <blake@blakestevenson.me>
2018-11-29 19:09:13 -0600

    Base class

---
+++ class.payments.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Blake
+ * Date: 11/29/2018
+ * Time: 7:06 PM
+ */
+
+class payments {
+    public function process_payment(array $params, $gateway) {
+        
+    }
+}
